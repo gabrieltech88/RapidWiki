09322321070df2286773a8aee8d11f3a9f7a2b43
Gabriel Ramos <gabrieltech209@gmail.com>
2026-09-08 17:04:51 -0300

    Second Commit

--- RapidWiki.Api/Controllers/AuthController.cs
+++ RapidWiki.Api/Controllers/AuthController.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc;
+
 namespace RapidWiki.Api.Controllers
 {
    [ApiController]
@@ -13,33 +15,7 @@ namespace RapidWiki.Api.Controllers
        [HttpPost("register")]
        public async Task<IActionResult> Register([FromBody] RegisterRequest request)
        {
-           var user = new IdentityUser<Guid>
-           {
-               UserName = request.Email,
-               Email = request.Email
-           };
-
-           var result = await _userManager.CreateAsync(user, request.Password);
-
-           if (!result.Succeeded)
-           {
-               return BadRequest(result.Errors);
-           }
-
-           return Ok();
-       }
-
-       [HttpPost("login")]
-       public async Task<IActionResult> Login([FromBody] LoginRequest request)
-       {
-           var result = await _signInManager.PasswordSignInAsync(request.Email, request.Password, false, false);
-
-           if (!result.Succeeded)
-           {
-               return Unauthorized();
-           }
-
-           return Ok();
+           
        }
    }
 }
